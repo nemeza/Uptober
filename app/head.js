70fe393bfaf2b32a2ee7de5790c684c5fe9db64b
nemeza <nemezancev@gmail.com>
2024-10-01 00:04:55 +0300

    Update head.js

--- app/head.js
+++ app/head.js
@@ -1,6 +1,6 @@
 const Head = () => (
   <>
-    <title>BranCoin</title>
+    <title>Uptober</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/bcstamp.png" />
   </>
